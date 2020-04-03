f5ae2a42ca90d86e3f093ca4fa833ea60b3156ca
hipenpen <63127322+hipenpen@users.noreply.github.com>
2020-04-04 06:32:15 +0900

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,39 @@
+
+function play_se(){
+	var warning = new Audio('Warning-Alarm02-2L.mp3');
+	var voice = new Audio('voice.wav');
+	warning.play();
+	voice.play();
+	navigator.vibrate([200,100,200,100,900,1])
+
+}
+ $(function(){
+ 	
+ 	var device = navigator.userAgent.match(/Windows|iPhone|Android|iPad/);
+ 	$('#device').text(device);
+ 	if (device == null) {
+ 		device='端末';
+ 	}
+
+ 	var time =60;
+ 	setInterval(function(){
+ 		time--;
+ 		$('#timer').text(time);
+ 	},1000);
+
+ 	$('.modal').modal({dismissible: false});
+ 	$('#alert').modal('open');
+ 	$('#close').click(function(){
+ 		$('#alert').modal('close');
+ 		play_se();
+ 	})
+
+ 
+ 	     
+
+    
+   
+
+
+ 	
+ });
